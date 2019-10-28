e7d3db3ac672fbb9f19696fa6299be0c78af8ad1
Stuart Creed <stuart.a.creed@gmail.com>
2019-10-28 22:27:11 +0000

    build

---
+++ confusion/build/precache-manifest.7543e805b756140d7899bc3644617300.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2277727a64539ca8d85ee2ac84b8e00",
+    "url": "/index.html"
+  },
+  {
+    "revision": "54ba31c229f6afc8a573",
+    "url": "/static/css/2.f815ad8f.chunk.css"
+  },
+  {
+    "revision": "c273b212abf0cb71dfc8",
+    "url": "/static/css/main.b57de112.chunk.css"
+  },
+  {
+    "revision": "54ba31c229f6afc8a573",
+    "url": "/static/js/2.8bdad8b0.chunk.js"
+  },
+  {
+    "revision": "c273b212abf0cb71dfc8",
+    "url": "/static/js/main.861d665f.chunk.js"
+  },
+  {
+    "revision": "cffad7811a97b1c3dd9a",
+    "url": "/static/js/runtime~main.7ac9f177.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
