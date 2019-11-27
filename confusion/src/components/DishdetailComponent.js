4108ee89511a7ba6d84a93433eefd9fbed10648a
Stuart Creed <stuart.a.creed@gmail.com>
2019-11-27 07:23:12 +0000

    React Animation Components

--- confusion/src/components/DishdetailComponent.js
+++ confusion/src/components/DishdetailComponent.js
@@ -6,6 +6,7 @@ import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Loading } from './LoadingComponent';
 import { baseUrl } from '../shared/baseUrl';
+import { FadeTransform, Fade, Stagger } from 'react-animation-components';
 
 const required = (val) => val && val.length;
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
@@ -96,23 +97,27 @@ function RenderComments({comments, addComment, dishId}){
 
        const cmt=comments.map(comment => {
          return(
-             <li key={comment.id}>
-                 <p>{comment.comment}</p>
-                 <p>- {comment.author},
-                 &nbsp; {new Intl.DateTimeFormat('en-US',{
-                      year: 'numeric',
-                      month: 'long',
-                      day: '2-digit'
-                 }).format(new Date(comment.date))}
-                 </p>
-             </li>
+            <Fade in>
+               <li key={comment.id}>
+                   <p>{comment.comment}</p>
+                   <p>- {comment.author},
+                   &nbsp; {new Intl.DateTimeFormat('en-US',{
+                        year: 'numeric',
+                        month: 'long',
+                        day: '2-digit'
+                   }).format(new Date(comment.date))}
+                   </p>
+               </li>
+            </Fade>
          )
      })
      return(
          <div className='col-12 col-md-5 m-1'>
              <h4>Comments</h4>
              <ul className='list-unstyled'>
+               <Stagger in>
                  {cmt}
+               </Stagger>
              </ul>
              <CommentForm dishId={dishId} addComment={addComment} />
          </div>
@@ -122,14 +127,20 @@ function RenderComments({comments, addComment, dishId}){
 function RenderDish({dish}){
         if(dish!=null){
             return(
-             <div className='col-12 col-md-5 m-1'>
-             <Card >
-              <CardImg top src={baseUrl + dish.image} alt={dish.name} />
-               <CardBody>
-                  <CardTitle>{dish.name}</CardTitle>
-                  <CardText>{dish.description}</CardText>
-              </CardBody>
-            </Card>
+            <div className='col-12 col-md-5 m-1'>
+            <FadeTransform
+                in
+                transformProps={{
+                    exitTransform: 'scale(0.5) translateY(-50%)'
+                }}>
+               <Card>
+                   <CardImg top src={baseUrl + dish.image} alt={dish.name} />
+                   <CardBody>
+                       <CardTitle>{dish.name}</CardTitle>
+                       <CardText>{dish.description}</CardText>
+                   </CardBody>
+               </Card>
+            </FadeTransform>
             </div>
             )
        }
